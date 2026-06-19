2312172b832ca4cfa4eacb7f172e30c4b8f49f03
Imanol Irizar <imanol.irizar13@gmail.com>
2026-06-19 21:00:14 +0200

    Allow entering towns and raids situated on water tiles

--- js/ui/world.js
+++ js/ui/world.js
@@ -180,8 +180,8 @@ export function renderWorldMap() {
         });
       }
 
-      // Allow entering town/raid if clicked on player's current coordinate (and not water)
-      if (x === STATE.party.worldX && y === STATE.party.worldY && tiles[y][x] !== 'water') {
+      // Allow entering town/raid if clicked on player's current coordinate (and not water, unless there is a location)
+      if (x === STATE.party.worldX && y === STATE.party.worldY && (tiles[y][x] !== 'water' || hasLocation)) {
         elCell.addEventListener('click', () => {
           tryEnterCurrentLocation();
         });
@@ -396,7 +396,7 @@ export function tryEnterCurrentLocation() {
   let locData = STATE.worldMap.locations[locKey];
   const terrain = STATE.worldMap.tiles[py][px];
 
-  if (terrain === 'water') {
+  if (terrain === 'water' && !locData) {
     logWorld('Cannot enter the deep sea.');
     return;
   }
